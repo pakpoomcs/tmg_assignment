2f90e38941b3912a8e1a1460e2ce30fffd6991f0
Pakpoom Sungtong <pakpoom.cs@gmail.com>
2022-12-02 22:51:12 +0700

    css mockup

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,32 +1,36 @@
-import React from 'react';
-import axios from 'axios';
-
+import React from 'react'
+import axios from 'axios'
+import '../css/ProductList.css'
 export default class PersonList extends React.Component {
-    state = {
-      products: []
-    }
-  
-    componentDidMount() {
-      axios.get(`https://asia-southeast1-msellercenter.cloudfunctions.net/testProducts?currentPage=1&pageSize=200`)
-        .then(res => {
-          const products = res.data.listProduct.items;
-          this.setState({ products });
-        })
-    }
-  
-    render() {
-      return (
-        <ul>
-          {
-            this.state.products
-              .map(x =>
-                
-                <li key={x.id}>{x.name}
-                <img class="prodThumbnails" alt={x.image.label} src={x.small_image.url}></img></li>
-              )
-          }
-        </ul>
-      )
-    }
+  state = {
+    products: [],
   }
-  
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://asia-southeast1-msellercenter.cloudfunctions.net/testProducts?currentPage=1&pageSize=200`
+      )
+      .then((res) => {
+        const products = res.data.listProduct.items
+        this.setState({ products })
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.products.map((x) => (
+          <li key={x.id}>
+            {x.name}
+            <img
+              className="prodThumbnails"
+              alt={x.image.label}
+              src={x.small_image.url}
+            ></img>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
